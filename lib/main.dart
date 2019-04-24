696922abfd994a791e485d89db39d8eb6e9ded9c
tiivanov <tihomir.v.ivanov@gmail.com>
2019-04-24 23:40:02 +0300

    dev

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ import './screens/dashboard.dart';
 import './screens/login.dart';
 import './screens/form.dart';
 import './screens/statements.dart';
+import './screens/request.dart';
 
 void main() {
   final auth = AuthModel();
@@ -66,7 +67,8 @@ class MyApp extends StatelessWidget {
           '/simulation': (context) => SimulationPage(),
           '/dashboard': (context) => Dashboard(),
           '/form': (context) => FormPage(),
-          '/statements': (context) => StatementsPage()
+          '/statements': (context) => StatementsPage(),
+          '/request':(context) => RequestPage()
         },
       ),
     );
